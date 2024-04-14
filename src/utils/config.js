a34949298a00fb79459c7bbb5889718d7c993e6f
Akshu9422 <akshat.wk@gmail.com>
2024-04-14 13:16:12 +0530

    initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+const predictedClassNames = [
+  "No symptoms detected!!",
+  "I am feeling like vomiting.",
+  "I had an injury.",
+  "I am having a headache.",
+  "I am having loose motions",
+  "I have high temperature of my body.",
+  "I am having cough and my throat is aching.",
+  "Not a registered gesture!!.",
+  "Not a registered gesture!!.",
+];
+
+export function getPredictedDisease(predictedClass) {
+  if (predictedClass === null) {
+    return predictedClassNames[0];
+  }
+
+  return predictedClassNames[predictedClass + 1];
+}
